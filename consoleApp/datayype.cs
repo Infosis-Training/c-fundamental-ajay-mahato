9e1c0503c4ced41f8fb7f6debbffe9787d86336a
ajay mahato <hateyoulove91@gmail.com>
2022-05-27 07:51:45 +0545

    learn about datatype

---
+++ consoleApp/datayype.cs
@@ -0,0 +1,14 @@
+class DataType
+{
+    byte age =23;     //0-255
+    short b=3534;
+    int c = 2523523;
+    long d = 23232525235;
+    float e = 2342.546f;
+    double f = 23423.456;
+    decimal g = 34.3452535m;
+    bool h =true;
+    char i ='N';
+    string j ="eklhielu";
+    int? value =null;
+}
